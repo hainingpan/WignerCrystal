e213e43fb8fc389ec3004a2b7bc303852e48be2f
Haining Pan <jack_pan1994@126.com>
2020-06-17 11:03:37 -0400

    temporarily disable iter.m mainTMD.m

--- iter.m
+++ iter.m
@@ -1,4 +1,4 @@
-parameters=mainTMD('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',4,'nu',[1,2]);
+parameters=mainTMD('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',4,'nu',[1,3]);
 tshell=3;
 Ushell=8;
 % [t,neighborlist]=t_calc_func(tshell,parameters);
