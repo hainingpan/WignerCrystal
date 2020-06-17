473272b6d6ed7947d27400e16acc5fcefbdd155a
Haining Pan <jack_pan1994@126.com>
2020-06-17 10:49:02 -0400

    iter

--- iter.m
+++ iter.m
@@ -1,4 +1,4 @@
-parameters=mainTMD('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',4,'nu',[2,3]);
+parameters=mainTMD('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',4,'nu',[1,2]);
 tshell=3;
 Ushell=6;
 % [t,neighborlist]=t_calc_func(tshell,parameters);
