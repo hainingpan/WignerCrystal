e39ef47781ca8d35a503a30abefca9faca62152c
Haining Pan <jack_pan1994@126.com>
2020-06-25 00:54:55 -0400

    Update mainTMD_2.m

--- mainTMD_2.m
+++ mainTMD_2.m
@@ -60,7 +60,7 @@ rotate=@(x) [cos(x) -sin(x);sin(x) cos(x)]; %rotate anticlockwise
 if parameters.nu==[1,2] 
     ailist={[0,0],[-1,0],[-1,1],[-2,1]};
     parameters.inner=cellfun(@(x) x(1)*parameters.aM1+x(2)*parameters.aM2,ailist,'UniformOutput',0);
-    parameters.spin0={[0,0,1],[0,0,1]};
+    parameters.spin0={[0,0,1],[0,0,-1]};
    am1index=[0,2];
    am2index=[2,0];
 end
