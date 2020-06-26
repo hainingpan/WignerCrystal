f2ed0ec0cfd5eebde9a97ba7580f8dec309c7efd
Haining Pan <jackpan1994@gmail.com>
2020-06-25 20:11:15 -0400

    add nu=1,5
    120 AF is favorable

--- V.m
+++ V.m
@@ -1,6 +1,6 @@
 function sum=V(nlist,Ulist,kx,ky,parameters)
 sum=0;
-for i=1:length(Ulist)
+parfor i=1:length(Ulist)
     n=nlist{i}(1)*parameters.aM1+nlist{i}(2)*parameters.aM2;
     sum=sum+Ulist(i)*exp(-1i*(kx.*n(1)+ky.*n(2)));
 end
