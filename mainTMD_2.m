a7bee8600b127f2689703ae043fdd3b54f13c310
Haining Pan <jack_pan1994@126.com>
2020-07-06 23:52:37 -0400

    update 3/12 as Ferro

--- mainTMD_2.m
+++ mainTMD_2.m
@@ -150,6 +150,14 @@ if parameters.nu==[1,4]
    am2index=[-4,2];
 end
 
+if parameters.nu==[3,12]
+    ailist={[0,0],[-2,2],[-4,4],[-1,2],[-2,3],[-3,4],[-1,1],[-3,3],[-5,5],[-2,1],[-3,2],[-4,3]};
+    parameters.inner=cellfun(@(x) x(1)*parameters.aM1+x(2)*parameters.aM2,ailist,'UniformOutput',0);
+%     parameters.spin0={[1,0,0],[cos(2*pi/3),sin(2*pi/3),0],[cos(4*pi/3),sin(4*pi/3),0]};
+   parameters.spin0={[0,0,1],[0,0,1],[0,0,1]};
+   am1index=[-2,4];
+   am2index=[-4,2];
+end
 
 %For tetrahedron spin
 if parameters.nu==[2,4] 
