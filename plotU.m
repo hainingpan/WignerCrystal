0ee5a64fb0ae2987c528d48b3884598653043d32
Haining Pan <jackpan1994@gmail.com>
2020-07-10 14:16:04 -0400

    Create plotU.m

---
+++ plotU.m
@@ -0,0 +1,8 @@
+Ur=cellfun(@(x)x(1),U);
+Ushell=35;
+nlist=cellfun(@(x) norm(x{1}(1)*parameters.aM1+x{1}(2)*parameters.aM2),neighborlist(1:Ushell+1),'UniformOutput',false);
+nlist=cell2mat(nlist);
+alpha=0.00729735;
+Ur2=alpha./(nlist)-alpha./sqrt(nlist.^2+(parameters.d)^2);
+figure;plot(nlist/parameters.aM,Ur(1:Ushell+1),'.',nlist/parameters.aM,Ur2,'.');
+legend('origin','fit')
