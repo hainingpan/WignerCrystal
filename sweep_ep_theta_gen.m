fe26604300ad6ccd75d77674d21e78e4a38d1d7f
Haining Pan <jack_pan1994@126.com>
2020-07-19 10:40:56 -0400

    back to 60

--- sweep_ep_theta_gen.m
+++ sweep_ep_theta_gen.m
@@ -12,7 +12,7 @@ V2={};
 energylist={};
 for thetai=1:Ntheta
     disp(thetai);
-    parameters=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',thetalist(thetai),'d',10e-9*5.076e6,'nu',nu);
+    parameters=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',thetalist(thetai),'d',60e-9*5.076e6,'nu',nu);
     [t,neighborlist]=t_calc_func(tshell,parameters);
     U=U_calc_func_2(Ushell,parameters);
     
