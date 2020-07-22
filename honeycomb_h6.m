bab3232ca463b6fd02462cd0ecbe7224315fa861
Haining Pan <jack_pan1994@126.com>
2020-07-22 16:48:31 -0400

    add honeycomb, tU sweep

---
+++ honeycomb_h6.m
@@ -0,0 +1,26 @@
+function [sum,eigvec2,eigval]=honeycomb_h6(phi,k,parameters)
+a1=-parameters.aM1;
+a2=parameters.aM2;
+a3=-(a1+a2);
+b1=a2-a3;
+b2=a3-a1;
+b3=a1-a2;
+
+t2=.1;
+ham11=[2*t2*(cos(k*b1'+phi)+cos(k*b2'+phi)+cos(k*b3'+phi)),exp(1i*k*a1')+exp(1i*k*a2')+exp(1i*k*a3'),0;
+    exp(-1i*k*a1')+exp(-1i*k*a2')+exp(-1i*k*a3'),2*t2*(cos(k*b1'-phi)+cos(k*b2'-phi)+cos(k*b3'-phi)),0;
+    0,0,100];
+ham22=100*eye(3);
+ham22(3,3)=-10;
+ham=[ham11,zeros(3);zeros(3),ham22];
+[eigvec,eigval]=eig(ham);
+eigval=diag(eigval);
+eigvec2=eigvec;
+[~,I]=sort(eigval);
+eigvec=eigvec(:,I);
+sum=0; %alpha',alpha
+for i=1:3
+    sum=sum+conj(eigvec(:,i))*eigvec(:,i).';
+end
+
+end
---
+++ tU_ep_theta_gen.m
@@ -0,0 +1,19 @@
+%Sweep for Wigner Crystal as a function epsilon and theta
+function tU_ep_theta_gen(thetalist)
+
+Ntheta=length(thetalist);
+
+tshell=3;
+Ushell=length(generate_neighbor(100));
+
+t={};
+U={};
+for thetai=1:Ntheta
+    disp(thetai);
+    parameters=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',thetalist(thetai),'d',60e-9*5.076e6,'nu',[1,2]);
+    [t{thetai},neighborlist]=t_calc_func(tshell,parameters);
+    U{thetai}=U_calc_func_2(Ushell,parameters);    
+end
+save(sprintf('tU_theta(%0.2f,%0.2f,%d).mat',thetalist(1),thetalist(end),Ntheta),'t','U');
+end
+
