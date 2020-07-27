8c28c57b3ed494b20418ef18bd7f14a7e88031d8
Haining Pan <jack_pan1994@126.com>
2020-07-27 18:56:17 -0400

    update sweepepsilon

--- sweepepsilon.m
+++ sweepepsilon.m
@@ -2,7 +2,7 @@ function [final,spin,gap,innergap,i,ch]=sweepepsilon(epsilon,kxlist,kylist,param
 parameters.V1=parameters.V1/epsilon;
 parameters.V2=parameters.V2/epsilon;
 
-if ismember(parameters.nu,[[4,8];[5,10];[2,2];[4,4];[7,28];[8,32]])
+if ismember(parameters.nu,[[4,8];[5,10];[2,2];[4,4];[7,28];[8,32]],'row')
     [ave,V2ave]=average_kagome(parameters.phi,parameters.s1,kxlist,kylist,parameters);
     [energyall,wfall]=energyMF_2(ave,V2ave,parameters);
 else
@@ -10,7 +10,7 @@ else
         [ave,V2ave]=average_honeycomb(kxlist,kylist,parameters);
         [energyall,wfall]=energyMF_2(ave,V2ave,parameters);
     else
-        if ismember(parameters.nu,[[4,6],[6,6]])
+        if ismember(parameters.nu,[[4,6];[6,6]],'row')
             [ave,V2ave]=average_honeycomb2(kxlist,kylist,parameters);
             [energyall,wfall]=energyMF_2(ave,V2ave,parameters);        
         else
