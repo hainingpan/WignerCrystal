1db6b48e8143a9d845fe886697553df385c1766f
Haining Pan <jackpan1994@gmail.com>
2020-11-13 11:07:25 -0500

    Update sweep_ep_theta.m

--- sweep_ep_theta.m
+++ sweep_ep_theta.m
@@ -4,6 +4,8 @@ function sweep_ep_theta(nu,epsilonlist,thetalist,Vz,filename)
 Ntheta=length(thetalist);
 Nep=length(epsilonlist);
 energylist={};
+V1={};
+V2={};
 % filename='phase4,12_theta(3.00,5.00,51).mat';
 load(filename);
 % load(sprintf('phase1,2_theta(%.2f,%.2f,%d)_d60.mat',thetalist(1),thetalist(end),Ntheta));
