42108378de1841dbfee232e99331768e5c37962a
Haining Pan <jack_pan1994@126.com>
2020-11-19 12:08:28 -0500

    update hole in sweep_ep_theta

--- sweep_ep_theta.m
+++ sweep_ep_theta.m
@@ -1,5 +1,5 @@
 %Sweep for Wigner Crystal as a function epsilon and theta
-function sweep_ep_theta(nu,epsilonlist,thetalist,Vz,filename)
+function sweep_ep_theta(nu,epsilonlist,thetalist,Vz,hole,filename)
 
 Ntheta=length(thetalist);
 Nep=length(epsilonlist);
@@ -9,7 +9,7 @@ V2={};
 % filename='phase4,12_theta(3.00,5.00,51).mat';
 load(filename);
 % load(sprintf('phase1,2_theta(%.2f,%.2f,%d)_d60.mat',thetalist(1),thetalist(end),Ntheta));
-param=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',3,'d',60e-9*5.076e6,'nu',nu,'Vz',Vz);
+param=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',3,'d',60e-9*5.076e6,'nu',nu,'Vz',Vz,'hole',hole);
 n=27*(length(param.Q)<8)+15*(length(param.Q)>=8)*(length(param.Q)<16)+9*(length(param.Q)>=16);
 final=zeros(Ntheta,Nep);
 gap=zeros(Ntheta,Nep);
@@ -50,6 +50,6 @@ for thetai=1:Ntheta
 %         disp(epi)
     end
 end
-save(sprintf('phase%d,%d_Vz(%0.1f).mat',nu(1),nu(2),Vz),'nu','final','spin','epsilonlist','gap','innergap','thetalist','finali','ch');
+save(sprintf('phase%d,%d_Vz(%0.1f)_h(%d).mat',nu(1),nu(2),Vz,hole),'nu','final','spin','epsilonlist','gap','innergap','thetalist','finali','ch');
 end
 
