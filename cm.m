83eff5de397134080baa7efa129b67da20d8c888
Haining Pan <jack_pan1994@126.com>
2020-11-21 17:04:47 -0500

    change floor to round

--- cm.m
+++ cm.m
@@ -1,6 +1,6 @@
 %find the common multiplier for n
 function n=cm(delta)
-delta=floor(delta); 
+delta=round(delta); 
 nlist=3*find(arrayfun(@(k)mod(3^3*k^2,delta),1:20)==0);
 n1=nlist(nlist>=21);
 n=n1(1);
