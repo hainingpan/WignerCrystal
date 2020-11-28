fdf371a4bc0546830c236670ad60cb835f5bf8a1
Haining Pan <jack_pan1994@126.com>
2020-11-28 18:00:57 -0500

    fix V

--- V.m
+++ V.m
@@ -1,8 +1,9 @@
 function sum=V(nlist,Ulist,kx,ky,parameters)
 %assume the first element is U0
-% sum=Ulist(1);
-sum=0;
-for i=1:length(Ulist)
+% sum=Ulist(1)*ones(size(kx));
+sum=Ulist(1);
+% sum=0;
+for i=2:length(Ulist)
     n=nlist{i}(1)*parameters.aM1+nlist{i}(2)*parameters.aM2;
 %     if (n(1))==0 & (n(2))==0
 %         sum=sum+Ulist(i)*exp(-1i*(kx.*0+ky.*0));
