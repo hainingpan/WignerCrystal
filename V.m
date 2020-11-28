48020eb91e19b907963774891d95cec0165bef2e
Haining Pan <jack_pan1994@126.com>
2020-11-28 12:51:41 -0500

    change to start from 1 in V.m

--- V.m
+++ V.m
@@ -1,7 +1,7 @@
 function sum=V(nlist,Ulist,kx,ky,parameters)
 %assume the first element is U0
-sum=Ulist(1);
-for i=2:length(Ulist)
+% sum=Ulist(1);
+for i=1:length(Ulist)
     n=nlist{i}(1)*parameters.aM1+nlist{i}(2)*parameters.aM2;
 %     if (n(1))==0 & (n(2))==0
 %         sum=sum+Ulist(i)*exp(-1i*(kx.*0+ky.*0));
