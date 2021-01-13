bafbd39923b5642d3590aa27bdb239c38b95887c
Haining Pan <jackpan1994@gmail.com>
2021-01-13 15:22:08 -0500

    Update Ucutoff.m

--- Ucutoff.m
+++ Ucutoff.m
@@ -1,10 +1,14 @@
 % Ur2=13.6./(rlist*3.28e-10/parameters.theta/5.29177210903e-11)*2...
 % -13.6./(sqrt((rlist*3.28e-10/parameters.theta).^2+(1*3.28e-10/parameters.theta).^2)/5.29177210903e-11)*2;
-Ur=cellfun(@(x)x(1),U);
-
+Ur=cellfun(@(x)real(x(1)),U);
+neighbor=arrayfun(@(i) norm(neighborlist{i}{1}*[0,-1;sqrt(3)/2,-1/2]),1:length(U));
 alpha=0.00729735;
 d=parameters.d;
-rlist=1:0.1:9;
+rlist=0.5:0.1:20;
 Ur2=alpha./(rlist*parameters.aM)-alpha./(sqrt((rlist*parameters.aM).^2+parameters.d.^2));
 
-figure;plot(neighbor(2:length(Ur)),Ur(2:end),rlist,Ur2)
+figure;
+plot(rlist,Ur2);
+hold on;
+plot(neighbor(1:length(Ur)),Ur(1:end),'.');
+xlim([0,20])
