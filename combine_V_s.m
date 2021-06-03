22fea8fdd168dc825c5483bc7144fa4e4d178aa4
Haining Pan <jack_pan1994@126.com>
2021-06-03 02:25:04 -0400

    add combine_V_s and sweep_s_theta

---
+++ combine_V_s.m
@@ -0,0 +1,21 @@
+%combine V for theta
+% thetalist=3:0.04:5;
+thetalist=[4,5];
+slist=30:10:300;
+% slist=[30,60];
+V1store={};
+V2store={};
+energyliststore={};
+for thetai=1:length(thetalist)
+    tmp=load(sprintf('phase1,1_theta%0.2f_h1_s%d,%d.mat',thetalist(thetai),slist(1),slist(end)));
+    for si=1:length(slist)
+        V1store{thetai}{si}=tmp.V1{si};
+        V2store{thetai}{si}=tmp.V2{si};
+        energyliststore{thetai}{si}=tmp.energylist{si};
+    end
+end
+% clear V1 V2 energylist
+V1=V1store;
+V2=V2store;
+energylist=energyliststore;
+save(sprintf('phase1,1_theta(%0.2f,%0.2f,%d)_h1_s(%d,%d,%d).mat',thetalist(1),thetalist(end),length(thetalist),slist(1),slist(end),length(slist)),'V1','V2','energylist','-v7.3');
---
+++ sweep_s_theta.m
@@ -0,0 +1,59 @@
+function sweep_s_theta(nu,slist,thetalist,hole,perturb,perturbnear,filename,epsilon,Ush)
+
+Ntheta=length(thetalist);
+Ns=length(slist);
+energylist={};
+V1={};
+V2={};
+% filename='phase4,12_theta(3.00,5.00,51).mat';
+load(filename);
+% load(sprintf('phase1,2_theta(%.2f,%.2f,%d)_d60.mat',thetalist(1),thetalist(end),Ntheta));
+param=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',3,'d',inf,'nu',nu,'hole',hole,'perturb',perturb,'perturbnear',perturbnear);
+if perturb==1
+%     n=cm(abs(1/(1-nu(1)/nu(2))));
+    n=cm(param.nu(2)/gcd(param.nu(1),param.nu(2)),param);
+else
+    n=27*(length(param.Q)<8)+15*(length(param.Q)>=8)*(length(param.Q)<16)+9*(length(param.Q)>=16);
+end
+final=zeros(Ntheta,Ns);
+gap=zeros(Ntheta,Ns);
+innergap=zeros(Ntheta,Ns);
+finali=zeros(Ntheta,Ns);
+ch=zeros(Ntheta,Ns);
+parfor thetai=1:Ntheta
+    for si=1:Ns
+        parameters=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',thetalist(thetai),'d',inf,'s',slist(si)*1e-9*5.076e6,'nu',nu,'hole',hole,'perturb',perturb,'perturbnear',perturbnear);
+        kxlist=zeros(1,n^2);
+        kylist=zeros(1,n^2);
+        counter=1;
+        for xindex=1:n
+            for yindex=1:n
+                ux=(2*xindex-n-1)/(2*n);
+                uy=(2*yindex-n-1)/(2*n);
+                klist=ux*parameters.bm1+uy*parameters.bm2;
+                kxlist(counter)=klist(1);
+                kylist(counter)=klist(2);
+                counter=counter+1;
+            end
+        end
+        kxlist=kxlist';
+        kylist=kylist';
+
+        parameters.N=length(kxlist);
+        kxbasis=cell(1,length(parameters.Q));
+        kybasis=cell(1,length(parameters.Q));
+        for i=1:length(parameters.Q)
+            kxbasis{i}=kxlist+parameters.Q{i}(1);
+            kybasis{i}=kylist+parameters.Q{i}(2);
+        end
+        parameters.energylist=energylist{thetai}{si};
+
+        parameters.V1=V1{thetai}{si};
+        parameters.V2=V2{thetai}{si};
+        [final(thetai,si),spin(:,:,thetai,si),gap(thetai,si),innergap(thetai,si),finali(thetai,si),ch(thetai,si)]=sweepd(epsilon,kxlist,kylist,parameters);
+    end
+end
+save(sprintf('phase%d,%d_h%d_s_ep%d_U%d.mat',nu(1),nu(2),hole,epsilon,Ush),'nu','final','spin','slist','gap','innergap','thetalist','finali','ch');
+
+end
+
