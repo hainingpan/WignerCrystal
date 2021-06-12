9f92d475b2464fd7a645298b5ae9431ed849a9f2
Haining Pan <jack_pan1994@126.com>
2021-06-12 01:14:20 -0400

    Update combine_V.m

--- combine_V.m
+++ combine_V.m
@@ -1,7 +1,8 @@
+function combine_V(nu,Ushell)
 %combine V for theta
 thetalist=3:0.04:5;
-for thetai=1:length(thetalist)    
-    load(sprintf('phase4,12_theta(%0.2f,%0.2f,1).mat',thetalist(thetai),thetalist(thetai)));
+for thetai=1:length(thetalist)        
+    load(sprintf('phase%d,%d_theta(%0.2f,%0.2f,1)_h(1)_U%d.mat',nu(1),nu(2),thetalist(thetai),thetalist(thetai),Ushell));
     V1store{thetai}=V1{1};
     V2store{thetai}=V2{1};
     energyliststore{thetai}=energylist{1};
@@ -10,4 +11,4 @@ clear V1 V2 energylist
 V1=V1store;
 V2=V2store;
 energylist=energyliststore;
-save(sprintf('phase4,12_theta(%0.2f,%0.2f,%d).mat',thetalist(1),thetalist(end),length(thetalist)),'V1','V2','energylist','-v7.3');
+save(sprintf('phase%d,%d_theta(%0.2f,%0.2f,%d)_h(1)_U%d.mat',nu(1),nu(2),thetalist(1),thetalist(end),length(thetalist),Ushell),'V1','V2','energylist','-v7.3');
