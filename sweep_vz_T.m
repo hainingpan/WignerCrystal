54dfff992817221910cdaf2d228c601d97c31fe2
Haining Pan <jack_pan1994@126.com>
2021-08-04 00:36:27 -0400

    Update sweep_vz_T.m

--- sweep_vz_T.m
+++ sweep_vz_T.m
@@ -3,17 +3,14 @@ function sweep_vz_T(nu,vzlist,Tlist,theta,hole,perturb,perturbnear,filename,epsi
     NT=length(Tlist);
     Nvz=length(vzlist);
     energylist={};
-    thetalist=linspace(3,5,51);
-    assert(sum(thetalist==theta)==1,'Theta=%.2f not found',theta)
-    thetai=find(thetalist==theta);
+    % thetalist=linspace(3,5,51);
+    % assert(sum(thetalist==theta)==1,'Theta=%.2f not found',theta)
+    % thetai=find(thetalist==theta);
     V1={};
     V2={};
-    % filename='phase4,12_theta(3.00,5.00,51).mat';
     load(filename);
-    % load(sprintf('phase1,2_theta(%.2f,%.2f,%d)_d60.mat',Tlist(1),Tlist(end),NT));
     param=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',theta,'T',0,'d',inf*5.076e6,'nu',nu,'hole',hole,'perturb',perturb,'perturbnear',perturbnear);
     if perturb==1
-    %     n=cm(abs(1/(1-nu(1)/nu(2))));
         n=cm(param.nu(2)/gcd(param.nu(1),param.nu(2)),param);
     else
         n=27*(length(param.Q)<8)+15*(length(param.Q)>=8)*(length(param.Q)<16)+9*(length(param.Q)>=16);
@@ -25,7 +22,7 @@ function sweep_vz_T(nu,vzlist,Tlist,theta,hole,perturb,perturbnear,filename,epsi
     ch=zeros(NT,Nvz);
     parfor Ti=1:NT
         for vzi=1:Nvz
-            parameters=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',theta,'T',Tlist(thetai),'d',inf,'vz',vzlist(vzi),'nu',nu,'hole',hole,'perturb',perturb,'perturbnear',perturbnear);
+            parameters=mainTMD_2('m',0.45,'psi',-0.3329/(2*pi)*360,'V',4.428,'w',20,'theta',theta,'T',Tlist(Ti),'d',inf,'vz',vzlist(vzi),'nu',nu,'hole',hole,'perturb',perturb,'perturbnear',perturbnear);
             kxlist=zeros(1,n^2);
             kylist=zeros(1,n^2);
             counter=1;
@@ -49,14 +46,14 @@ function sweep_vz_T(nu,vzlist,Tlist,theta,hole,perturb,perturbnear,filename,epsi
                 kxbasis{i}=kxlist+parameters.Q{i}(1);
                 kybasis{i}=kylist+parameters.Q{i}(2);
             end
-            parameters.energylist=energylist{thetai}{vzi};
+            parameters.energylist=energylist{vzi};
     
-            parameters.V1=V1{thetai}{vzi};
-            parameters.V2=V2{thetai}{vzi};
+            parameters.V1=V1{vzi};
+            parameters.V2=V2{vzi};
             [final(Ti,vzi),spin(:,:,Ti,vzi),gap(Ti,vzi),innergap(Ti,vzi),finali(Ti,vzi),ch(Ti,vzi)]=sweepd(epsilon,kxlist,kylist,parameters);
         end
     end
-    save(sprintf('phase%d,%d_h%d_vz_ep%d_theta%.2f_U%d.mat',nu(1),nu(2),hole,epsilon,thetalist(thetai),Ush),'nu','final','spin','vzlist','gap','innergap','Tlist','finali','ch');
+    save(sprintf('phase%d,%d_h%d_vz_ep%d_theta%.2f_U%d.mat',nu(1),nu(2),hole,epsilon,theta,Ush),'nu','final','spin','vzlist','gap','innergap','Tlist','finali','ch');
     
     end
     
